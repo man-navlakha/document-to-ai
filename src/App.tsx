1ae15c8cb8c5de63af9cc3ea882ffaf7ba787730
Man Navlakha <mannavlakha1021@gmail.com>
2025-05-02 02:06:06 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/chat" element={<Chat />} />
-          <Route path="/url" element={<Url />} />
+          <Route path="/url/:url" element={<Url />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
